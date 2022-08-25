2e2f9dc7d821fe71385726643cd9665931b1bb1a
Diego Bahamondes <d.bahamondes@duocuc.cl>
2022-08-25 00:14:14 -0400

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
+import heavyRotation from '../../assets/mockdata/heavyRotation.json';
+import jumpBackin from '../../assets/mockdata/jumpbackIn.json';
+
+
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
 
-  constructor() {}
+  data = [
+    {
+      title: 'Recently played',
+      albums: recentlyPlayed
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation
+    },
+    {
+      title: 'Jump back in',
+      albums: jumpBackin
+    }
+  ];
 
+  opts = {
+    slidesPerView: 2.4,
+    slidesOffserBefore: 20,
+    spaceBetween: 20,
+    freeMode: true
+  }
+  
+  constructor() { }
+    //funcion para arreglar las imagenes
+    dasherize(string) {
+      return string.replace(/[A-Z]/g, function (char, index) {
+        return (index !== 0 ? '-' : '') + char.toLowerCase();
+      });
+  };
 }
